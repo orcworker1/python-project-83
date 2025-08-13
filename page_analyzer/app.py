1bc5c172ce7daed253a32defac01bb7a104be4bc
Валентин Алтабасов <valenjir@gmail.com>
2025-08-13 22:28:24 +0300

    add database and new route

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,10 +1,21 @@
-import os
-from flask import Flask, render_template
+import psycopg2, os , validators
+from dotenv import load_dotenv
+from flask import Flask, render_template, request , flash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+load_dotenv()
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/urls' , methods=['post'])
+def urls_add():
+    urls = request.form.get('url')
+    if not validators.url(urls):
+        flash('Некоректный URL', 'DANGER')
+        return render_template('index.html'), 422
+    pass

e91d8da1eecb8146feb55662dd3cc0de70e3080d
Валентин Алтабасов <valenjir@gmail.com>
2025-09-04 22:58:35 +0300

    fix ruff

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,38 +1,40 @@
-import psycopg2
 import os
-import validators
-import requests
-from dotenv import load_dotenv
-from flask import Flask, render_template, request , flash, redirect , url_for
-from psycopg2.extras import RealDictCursor
 from datetime import datetime
-from page_analyzer.parse import parser_h1, parser_title, parser_description
 from urllib.parse import urlparse
+
+import psycopg2
+import requests
+import validators
+from dotenv import load_dotenv
+from flask import Flask, flash, redirect, render_template, request, url_for
+from psycopg2.extras import RealDictCursor
+
+from page_analyzer.parse import parser_description, parser_h1, parser_title
 
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_connection():
     return psycopg2.connect(DATABASE_URL)
 
 def normalize_url(url):
     normalize = urlparse(url)
-    return f'{normalize.scheme}://{normalize.netloc}'
+    return f"{normalize.scheme}://{normalize.netloc}"
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/urls')
+@app.route("/urls")
 def urls():
     with get_connection() as conn:
         with conn.cursor(cursor_factory=RealDictCursor) as curs:
-            curs.execute('''
+            curs.execute("""
     SELECT 
         u.id,
         u.name,
@@ -49,57 +51,57 @@ def urls():
         ORDER BY url_id, created_at DESC
     ) latest ON u.id = latest.url_id
     ORDER BY u.created_at DESC;
-''')
+""")
             result = curs.fetchall()
-    return render_template('urls.html', urls=result)
+    return render_template("urls.html", urls=result)
 
 
-@app.route('/urls' , methods=['post'])
+@app.route("/urls" , methods=["post"])
 def urls_add():
-    urls = request.form.get('url')
+    urls = request.form.get("url")
     if not validators.url(urls):
-        flash('Некорректный URL', 'danger')
-        return render_template('index.html'), 422
+        flash("Некорректный URL", "danger")
+        return render_template("index.html"), 422
     normalized_url = normalize_url(urls)
     with get_connection() as conn:
         with conn.cursor(cursor_factory=RealDictCursor) as curs:
-            curs.execute('SELECT id from urls WHERE name =%s;',(normalized_url,))
+            curs.execute("SELECT id from urls WHERE name =%s;",(normalized_url,))
             result = curs.fetchone()
             if result:
-                flash('Страница уже существует', 'info')
-                return  redirect(url_for('show_url', id=result['id']))
-            curs.execute('INSERT INTO urls (name, created_at)'
-                         'VALUES (%s,%s) RETURNING id;',
+                flash("Страница уже существует", "info")
+                return  redirect(url_for("show_url", id=result["id"]))
+            curs.execute("INSERT INTO urls (name, created_at)"
+                         "VALUES (%s,%s) RETURNING id;",
                          (normalized_url,datetime.now()))
-            url_id = curs.fetchone()['id']
+            url_id = curs.fetchone()["id"]
             conn.commit()
-            flash('Страница успешно добавлена', 'success')
-            return redirect(url_for('show_url', id=url_id))
+            flash("Страница успешно добавлена", "success")
+            return redirect(url_for("show_url", id=url_id))
 
 
-@app.route('/urls/<int:id>')
+@app.route("/urls/<int:id>")
 def show_url(id):
     with get_connection() as conn:
         with conn.cursor(cursor_factory=RealDictCursor) as curs:
-            curs.execute('SELECT id , name , created_at FROM urls WHERE id =%s', (id,))
+            curs.execute("SELECT id , name , created_at FROM urls WHERE id =%s", (id,))
             url = curs.fetchone()
-            curs.execute('SELECT id, url_id, status_code, h1, title, description, created_at FROM url_checks WHERE url_id =%s ORDER BY created_at DESC', (id,))
+            curs.execute("SELECT id, url_id, status_code, h1, title, description, created_at FROM url_checks WHERE url_id =%s ORDER BY created_at DESC", (id,))
             check = curs.fetchall()
-    return  render_template('urls_show.html', show=url, checks=check)
+    return  render_template("urls_show.html", show=url, checks=check)
 
 
-@app.route('/urls/<int:id>/checks', methods=['post'])
+@app.route("/urls/<int:id>/checks", methods=["post"])
 def add_check(id):
     with get_connection() as conn:
         with conn.cursor(cursor_factory=RealDictCursor) as curs:
-            curs.execute('SELECT id, name FROM urls WHERE id =%s', (id,))
+            curs.execute("SELECT id, name FROM urls WHERE id =%s", (id,))
             url_data = curs.fetchone()
-            url_name = requests.get(url_data['name'])
+            url_name = requests.get(url_data["name"])
             if not url_data:
-                flash('Сайт не найден', 'danger')
-                return redirect(url_for('urls'))
+                flash("Сайт не найден", "danger")
+                return redirect(url_for("urls"))
             try:
-                status_code = requests.get(url_data['name'])
+                status_code = requests.get(url_data["name"])
                 status_code.raise_for_status()
             except requests.RequestException:
                 flash("Произошла ошибка при проверке", "danger")
@@ -115,12 +117,12 @@ def add_check(id):
                         VALUES (%s, %s, %s, %s, %s, %s)
                         """,
                         (id, status_code.status_code, h1, title, description,
-                         datetime.now())
+                         datetime.now()),
                     )
                     conn.commit()
-            flash('Страница успешно проверена', 'success')
+            flash("Страница успешно проверена", "success")
 
-    return redirect(url_for('show_url', id=id))
+    return redirect(url_for("show_url", id=id))
 
 
 
